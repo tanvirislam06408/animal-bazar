4c754b214577f0a410f07aa0adba99609f12c079
tanvir <mstanvirislam05@gmail.com>
2026-05-01 13:57:43 +0600

    fix tips layout

--- src/components/QurbaniTips.jsx
+++ src/components/QurbaniTips.jsx
@@ -36,7 +36,7 @@ const QurbaniTips = () => {
 
   return (
     <section className="py-12 mt-20 ">
-      <div className="max-w-6xl mx-auto px-4">
+      <div className="container mx-auto px-4">
         
   
         <h2 className="text-3xl font-bold text-center mb-8">
